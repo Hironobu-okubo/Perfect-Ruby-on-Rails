76544a9abc50d2903fc4a7e304ca610df8819f91
Hironobu-okubo <ee171068oh@gmail.com>
2022-10-07 19:45:36 +0900

    Test

---
+++ rails_test_sample/test/models/todo_test.rb
@@ -0,0 +1,17 @@
+require 'test_helper'
+
+class TodoTest < ActiveSupport::TestCase
+  test "the truth" do
+    assert true
+  end
+
+  test "sleep 1" do
+    sleep 3
+    assert true
+  end
+
+  test "sleep 2" do
+    sleep 3
+    assert true
+  end
+end
