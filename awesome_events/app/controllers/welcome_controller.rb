821c347030af5e6ac647fdde6872173b41cde58a
Hironobu-okubo <ee171068oh@gmail.com>
2022-10-08 21:26:18 +0900

    event create show

--- awesome_events/app/controllers/welcome_controller.rb
+++ awesome_events/app/controllers/welcome_controller.rb
@@ -1,6 +1,8 @@
 class WelcomeController < ApplicationController
-skip_before_action :authenticate
+  skip_before_action :authenticate
 
   def index
+    @events = Event.all
+    #@events = Event.where("start_at > ?", Time.zone.now).order(:start_at)
   end
 end
